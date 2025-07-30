2aeed97baa54acc6b4df7ed44da5eac851d83169
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-07-30 15:41:17 +0200

    test(*): write a failing test - create module skeleton and a placeholder route, then write an integration test

---
+++ src/modules/posts/posts.routes.ts
@@ -0,0 +1,5 @@
+import type { FastifyPluginAsync } from "fastify"
+
+// This is a placeholder so our test can import the file.
+// It doesn't do anything yet.
+export const postsRoutes: FastifyPluginAsync = async (fastify) => {}
