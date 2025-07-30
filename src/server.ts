04f6648d3d9e79c1367b834a4c0980e182d4d572
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-07-30 23:03:16 +0200

    feat(server): register reels module and routes in main Fastify app

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify"
 import { databasePlugin } from "./core/database/database.plugin"
 import { postsRoutes } from "./modules/posts/posts.routes"
+import { reelsRoutes } from "./modules/reels/reels.routes"
 
 const fastify = Fastify({
     logger: true,
@@ -8,8 +9,10 @@ const fastify = Fastify({
 
 // Register database plugin
 fastify.register(databasePlugin)
-// Register new posts routes
+// Register posts routes
 fastify.register(postsRoutes)
+// Register reels routes
+fastify.register(reelsRoutes)
 
 // Declare a default route
 fastify.get("/", function (request, reply) {
