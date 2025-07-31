d5bb913ee4112c0ebfb9d4b402c714619f8b0e80
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-07-31 20:26:16 +0200

    feat(server): add new highlightsRoutes route

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { databasePlugin } from "./core/database/database.plugin"
 import { postsRoutes } from "./modules/posts/posts.routes"
 import { reelsRoutes } from "./modules/reels/reels.routes"
 import { taggedRoutes } from "./modules/tagged/tagged.routes"
+import { highlightsRoutes } from "./modules/highlights/highlights.routes"
 
 const fastify = Fastify({
     logger: true,
@@ -15,7 +16,9 @@ fastify.register(postsRoutes)
 // Register reels routes
 fastify.register(reelsRoutes)
 // Register tagged posts routes
-fastify.register(reelsRoutes)
+fastify.register(taggedRoutes)
+// Register highlights routes
+fastify.register(highlightsRoutes)
 
 // Declare a default route
 fastify.get("/", function (request, reply) {
