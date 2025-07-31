c354754c422cb1d96b3d838f4b21aa2689ca9a84
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-07-31 20:22:10 +0200

    feat(routes.highlights): implement routes for fetching all highlights and retrieving a single highlight by id

---
+++ src/modules/highlights/highlights.routes.ts
@@ -0,0 +1,34 @@
+import type { FastifyInstance, FastifyPluginAsync } from "fastify"
+import { highlightsService } from "./highlights.service"
+
+const highlightsRoutes: FastifyPluginAsync = async (
+    fastify: FastifyInstance
+) => {
+    const service = highlightsService(fastify)
+
+    fastify.get("/highlights", async (request, reply) => {
+        const allHighlights = await service.getAll()
+
+        // Return a 200 OK status code with all highlights
+        return reply.code(200).send(allHighlights)
+    })
+    fastify.get<{
+        Params: { id: string }
+    }>("/highlights/:id", async (request, reply) => {
+        const { id } = request.params as { id: string }
+        const numericId = Number(id)
+
+        if (isNaN(numericId)) {
+            return reply
+                .code(400)
+                .send({ error: "Invalid ID format. Must be a number." })
+        }
+
+        const highlightById = await service.getById(numericId)
+
+        // Return a 200 OK status code for a highlight by id
+        return reply.code(200).send(highlightById)
+    })
+}
+
+export { highlightsRoutes }
