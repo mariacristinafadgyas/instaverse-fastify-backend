448df3d8499ab38faf6cab2019b7ab475d8a5a4a
mariacristinafadgyas <fadgyas.maria@yahoo.com>
2025-07-31 18:21:36 +0200

    feat(server): register the new taggedRoutes

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Fastify from "fastify"
 import { databasePlugin } from "./core/database/database.plugin"
 import { postsRoutes } from "./modules/posts/posts.routes"
 import { reelsRoutes } from "./modules/reels/reels.routes"
+import { taggedRoutes } from "./modules/tagged/tagged.routes"
 
 const fastify = Fastify({
     logger: true,
@@ -12,6 +13,8 @@ fastify.register(databasePlugin)
 // Register posts routes
 fastify.register(postsRoutes)
 // Register reels routes
+fastify.register(reelsRoutes)
+// Register tagged posts routes
 fastify.register(reelsRoutes)
 
 // Declare a default route
